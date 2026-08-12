83786d22ec2faf25a947b64232b4b0f094ba9f96
Budokai-Msii <114784512+budokai-msi@users.noreply.github.com>
2026-08-12 06:44:41 -0400

    Define canonical public contact channels

--- src/config/business.js
+++ src/config/business.js
@@ -2,6 +2,8 @@ export const BUSINESS = Object.freeze({
   name: "Simple IT SRQ",
   siteUrl: "https://simpleitsrq.com",
   email: "hello@simpleitsrq.com",
+  phoneDisplay: "(813) 434-3230",
+  phoneE164: "+18134343230",
   locationLabel: "Bradenton, Florida",
   serviceRegion: "Southwest Florida",
   supportHours: "Business hours",
@@ -10,6 +12,9 @@ export const BUSINESS = Object.freeze({
 export const CONTACT = Object.freeze({
   email: BUSINESS.email,
   mailto: `mailto:${BUSINESS.email}`,
+  phoneDisplay: BUSINESS.phoneDisplay,
+  tel: `tel:${BUSINESS.phoneE164}`,
+  sms: `sms:${BUSINESS.phoneE164}`,
   bookingPath: "/book",
   supportPath: "/support",
 });
